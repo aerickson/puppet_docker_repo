e7197b9ddee276cfddcabff7e5c4becc2027f228
Andrew Erickson <aerickson@mozilla.com>
2019-11-04 20:09:10 -0500

    notes

--- modules_mycompany/mycompany_webserver/manifests/init.pp
+++ modules_mycompany/mycompany_webserver/manifests/init.pp
@@ -38,7 +38,10 @@ class mycompany_webserver
 	  package => pip
 	}
 
-	# https://stackoverflow.com/questions/44439763/puppet-installing-pip-rather-than-pip3-packages-using-the-pip3-provider-only-on
+	# https://ask.puppet.com/question/25819/how-to-install-pip-packages/
+	# https://puppet.com/docs/puppet/latest/types/package.html#package-provider-pip3
+	# BUG: https://stackoverflow.com/questions/44439763/puppet-installing-pip-rather-than-pip3-packages-using-the-pip3-provider-only-on
+
 	package { 'zstandard':
 	  ensure   => '0.11.1',
 	  # require  => Package['python-pip', 'python-devel', 'openssl-devel', 'libffi-devel'],
