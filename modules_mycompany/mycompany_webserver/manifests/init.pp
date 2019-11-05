dfd165e96fd8c16215cc752146015a35abbf4ad7
Andrew Erickson <aerickson@mozilla.com>
2019-11-04 20:06:27 -0500

    wipo

--- modules_mycompany/mycompany_webserver/manifests/init.pp
+++ modules_mycompany/mycompany_webserver/manifests/init.pp
@@ -6,6 +6,43 @@
 class mycompany_webserver
 {
 
- class { 'apache': }
+	# call apache class
+ 	# class { 'apache': }
+
+ 	package { ['python3-pip']:
+      ensure => installed,
+ 	}
+
+	package { ['python-pip']:
+	  ensure => installed,
+	}
+
+	# package { 'pip':
+	#   require  => Package['python-pip'],
+	#   ensure   => latest,
+	#   provider => 'pip',
+	# }
+
+	# package { ['python-devel', 'openssl-devel', 'libffi-devel']:
+	#   ensure => installed,
+	# }
+
+	# need 
+	#
+    # export LC_ALL=C
+
+
+	package { 'zstandard':
+	  ensure   => '0.11.1',
+	  # require  => Package['python-pip', 'python-devel', 'openssl-devel', 'libffi-devel'],
+	  package => pip
+	}
+
+	# https://stackoverflow.com/questions/44439763/puppet-installing-pip-rather-than-pip3-packages-using-the-pip3-provider-only-on
+	package { 'zstandard':
+	  ensure   => '0.11.1',
+	  # require  => Package['python-pip', 'python-devel', 'openssl-devel', 'libffi-devel'],
+	  package => pip3
+	}
 
 }
